0570578f0d9d5f0acfbf6e260d8c970822342696
Michael Osthege <michael.osthege@outlook.com>
2025-11-15 02:28:23 +0100

    Implement interrupt-based  RPM measurement

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
+uint16_t pulseCount = 0;
+uint16_t interval = 200;
+unsigned long lastUpdate = 0;
+
+
+ICACHE_RAM_ATTR void countPulse() {
+    pulseCount++;
+}
 
 void setup()
 {
@@ -18,7 +26,7 @@ void setup()
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.cp437(true);
-
+    
     display.setCursor(0, 0);
     display.println("0123456789");
     display.setCursor(0, 8);
@@ -32,16 +40,31 @@ void setup()
     display.setCursor(0, 40);
     display.println(1234567890);
     display.display();
-
     delay(2000);
+
+    pinMode(D4, INPUT);
+    attachInterrupt(digitalPinToInterrupt(D4), countPulse, FALLING);
 }
 
 void loop()
 {
+    static unsigned long elapsed = millis() - lastUpdate;
+    static float rpm;
+
+    if (elapsed < interval)
+        return;
+
+    // 2 pulses per revolution!
+    rpm = (pulseCount * 30000.0) / elapsed;
+
+    Serial.printf(">pulses:%d\n", pulseCount);
+    Serial.printf(">RPM:%.1f\n", rpm);
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextSize(2);
-    display.printf("%04d RPM", (int)(millis() / 1000));
+    display.printf("%05d RPM", (int)rpm);
     display.display();
-    delay(1000);
+
+    pulseCount = 0;
+    lastUpdate = millis();
 }
